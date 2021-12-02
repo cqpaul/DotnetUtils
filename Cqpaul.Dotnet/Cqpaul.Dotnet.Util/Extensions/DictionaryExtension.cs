059edc1008ae8e637d5934f93b7cbde94b96034a
cqpaul <1547401163@qq.com>
2021-12-02 10:35:29 +0800

    Add DictionaryExtension

---
+++ Cqpaul.Dotnet/Cqpaul.Dotnet.Util/Extensions/DictionaryExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cqpaul.Dotnet.Util.Extensions
+{
+    public static class DictionaryExtension
+    {
+        /// <summary>
+        /// 将一个字典转化为 QueryString
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <param name="urlEncode"></param>
+        /// <returns></returns>
+        public static string ToUrlQueryString(this Dictionary<string, string> dict, bool urlEncode = true)
+        {
+            return string.Join("&", dict.Select(p => $"{(urlEncode ? p.Key?.UrlEncode() : "")}={(urlEncode ? p.Value?.UrlEncode() : "")}"));
+        }
+
+        /// <summary>
+        /// 移除空值项
+        /// </summary>
+        /// <param name="dict"></param>
+        public static void RemoveEmptyValueItems(this Dictionary<string, string> dict)
+        {
+            dict.Where(item => string.IsNullOrEmpty(item.Value)).Select(item => item.Key).ToList().ForEach(key =>
+            {
+                dict.Remove(key);
+            });
+        }
+
+    }
+}
--- Cqpaul.Dotnet/Cqpaul.Dotnet.Util/Extensions/StringExtension.cs
+++ Cqpaul.Dotnet/Cqpaul.Dotnet.Util/Extensions/StringExtension.cs
@@ -1,7 +1,16 @@
-﻿namespace Cqpaul.Dotnet.Util.Extensions
+﻿using System.Text;
+
+namespace Cqpaul.Dotnet.Util.Extensions
 {
     public static class StringExtension
     {
+        /// <summary>
+        /// 在文件名中，Append相关字符串
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="appendStr"></param>
+        /// <param name="Separator"></param>
+        /// <returns></returns>
         public static string AppendStrToFileName(this string fileName, string appendStr, string Separator)
         {
             List<string> nameParts = fileName.Split('.').ToList();
@@ -10,5 +19,20 @@
             string prefixFileName = string.Join(".", nameParts);
             return $"{prefixFileName}{Separator}{appendStr}.{fileSuffix}";
         }
+
+        /// <summary>
+        /// 将一个字符串 URL 编码
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string UrlEncode(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return "";
+            }
+            return System.Web.HttpUtility.UrlEncode(str, Encoding.UTF8);
+        }
+
     }
 }
