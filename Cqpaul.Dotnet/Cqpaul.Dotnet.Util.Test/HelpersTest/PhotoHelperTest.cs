3fae6bb9eb35f0fcae05de8df6bbed77c5378828
cqpaul <1547401163@qq.com>
2022-10-21 11:24:00 +0800

    Add PhotoHelper

---
+++ Cqpaul.Dotnet/Cqpaul.Dotnet.Util.Test/HelpersTest/PhotoHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using Cqpaul.Dotnet.Util.Extensions;
+using Cqpaul.Dotnet.Util.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cqpaul.Dotnet.Util.Test.HelpersTest
+{
+    public class PhotoHelperTest
+    {
+        [Fact]
+        public void GetPhotoMetaInfosTest()
+        {
+            var filePath = @"C:\Users\pauzhang\Desktop\MPaul\P70513-181220.jpg";
+            var result = PhotoHelper.GetPhotoMetaInfos(filePath);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void GetPhotoXYInfoTest()
+        {
+            var filePath = @"C:\Users\pauzhang\Desktop\MPaul\P70513-181220.jpg";
+            var result = PhotoHelper.GetPhotoXYInfo(filePath);
+            Assert.NotNull(result);
+        }
+
+    }
+}
---
+++ Cqpaul.Dotnet/Cqpaul.Dotnet.Util/Helpers/PhotoHelper.cs
@@ -0,0 +1,46 @@
+﻿using MetadataExtractor;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Cqpaul.Dotnet.Util.Helpers
+{
+    public static class PhotoHelper
+    {
+        /// <summary>
+        /// 读取文件的元数据信息
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static Dictionary<string, List<string?>> GetPhotoMetaInfos(string filePath)
+        {
+            Dictionary<string, List<string?>> photoMetaInfos = new Dictionary<string, List<string?>>();
+            IReadOnlyList<MetadataExtractor.Directory> metaDrectories = ImageMetadataReader.ReadMetadata(filePath);
+            foreach (var directory in metaDrectories)
+            {
+                foreach (Tag tag in directory.Tags)
+                {
+                    if (!photoMetaInfos.ContainsKey(tag.Name))
+                    {
+                        photoMetaInfos.Add(tag.Name, new List<string?>());
+                    }
+                    photoMetaInfos[tag.Name].Add(tag.Description);
+                }
+            }
+            return photoMetaInfos;
+        }
+
+        /// <summary>
+        /// 读取图片的经纬度信息
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static (string? Longitude, string? Latitude)? GetPhotoXYInfo(string filePath)
+        {
+            var allMetaInfo = PhotoHelper.GetPhotoMetaInfos(filePath);
+            if (allMetaInfo.Any(a => a.Key == "GPS Longitude"))
+            {
+                return (allMetaInfo["GPS Longitude"].First(), allMetaInfo["GPS Latitude"].First());
+            }
+            return null;
+        }
+
+    }
+}
