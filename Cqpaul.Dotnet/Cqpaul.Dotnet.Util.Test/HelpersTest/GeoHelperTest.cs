45b90f7f94ee7861707d5dc0e41fdad255fa1bbe
cqpaul <1547401163@qq.com>
2022-11-09 14:19:51 +0800

    Add GeoHelper

---
+++ Cqpaul.Dotnet/Cqpaul.Dotnet.Util.Test/HelpersTest/GeoHelperTest.cs
@@ -0,0 +1,24 @@
+﻿using Cqpaul.Dotnet.Util.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cqpaul.Dotnet.Util.Test.HelpersTest
+{
+    public class GeoHelperTest
+    {
+        [Fact]
+        public void Shp2GeojsonTest()
+        {
+            string sourcePath = @"C:\Users\pauzhang\Desktop\shapetemplate\面图形.shp";
+            string destPath = @"C:\Users\pauzhang\Desktop\shapetemplate\out.json";
+
+            var result = GeoHelper.Shp2Geojson(sourcePath, destPath);
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ Cqpaul.Dotnet/Cqpaul.Dotnet.Util/Helpers/GeoHelper.cs
@@ -0,0 +1,43 @@
+﻿using MaxRev.Gdal.Core;
+using OSGeo.GDAL;
+using OSGeo.OGR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cqpaul.Dotnet.Util.Helpers
+{
+    public static class GeoHelper
+    {
+        /// <summary>
+        /// Gdal 之 SHP 转 Geojson
+        /// </summary>
+        /// <param name="srcPath"></param>
+        /// <param name="destPath"></param>
+        public static bool Shp2Geojson(string srcPath, string destPath)
+        {
+            //添加所有配置
+            GdalBase.ConfigureAll();
+            //配置单个选项
+            Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "YES");//支持中文路径
+            Gdal.SetConfigOption("DXF_ENCODING", "UTF-8");
+
+            //读取文件，这里自动区别文件类型
+            using var ds = Ogr.Open(srcPath, 0);
+
+            //根据文件名创建驱动
+            using var dv = Ogr.GetDriverByName("GeoJSON");
+
+            //拷贝数据与转化
+            using var ret = dv.CopyDataSource(ds, destPath, null);
+
+            // 手动释放,否则destPath一直被占用
+            ret.FlushCache();
+            ret.Dispose();
+            return true;
+        }
+
+    }
+}
